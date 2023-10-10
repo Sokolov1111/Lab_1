b96a7678e08233738b249e334d49f79a7ae56f5a
Sokolov1111 <Irina_Sok@mail.ru>
2023-10-10 22:25:07 +0300

    Калькулятор с проверкой формата введенных операндов

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -7,34 +7,44 @@ class Calculator
         Console.WriteLine("Calculator");
 
         Console.WriteLine("Введите первое число");
-        double firstNum = Convert.ToDouble(Console.ReadLine());
+        string firstInput = Console.ReadLine();
 
         Console.WriteLine("Введите второе число");
-        double secondNum = Convert.ToDouble(Console.ReadLine());
+        string secondInput = Console.ReadLine();
 
         Console.WriteLine("Введите операцию из списка: '+', '-', '/', '*' ");
         char oper = Convert.ToChar(Console.ReadLine());
 
         double res = 0;
 
-        switch (oper)
+        double firstNum, secondNum;
+
+        if (!double.TryParse(firstInput, out firstNum) || !double.TryParse(secondInput, out secondNum))
         {
-            case '+': 
-                res = firstNum + secondNum; break;
-            case '-':
-                res = firstNum - secondNum; break;
-            case '/':
-                if (secondNum == 0)
-                {
-                    Console.WriteLine("Ошибка деления на 0 !!!");
-                    Environment.Exit(0);
-                    break;
-                } else
-                {
-                    res = firstNum / secondNum; break;
-                }                
-            case '*':
-                res = firstNum * secondNum; break;
+            Console.WriteLine("Введены неккоректные значения операндов");
+            Environment.Exit(0);
+        } else
+        {
+            switch (oper)
+            {
+                case '+':
+                    res = firstNum + secondNum; break;
+                case '-':
+                    res = firstNum - secondNum; break;
+                case '/':
+                    if (secondNum == 0)
+                    {
+                        Console.WriteLine("Ошибка деления на 0 !!!");
+                        Environment.Exit(0);
+                        break;
+                    }
+                    else
+                    {
+                        res = firstNum / secondNum; break;
+                    }
+                case '*':
+                    res = firstNum * secondNum; break;
+            }
         }
 
         Console.WriteLine("Результат операции: " +  res);
