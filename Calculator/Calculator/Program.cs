9eda160859780bbc45e74865e52785ae76e88feb
Sokolov1111 <Irina_Sok@mail.ru>
2023-10-10 21:30:53 +0300

    Добавлена 1ая версия калькулятора

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Calculator
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Calculator");
+
+        Console.WriteLine("Введите первое число");
+        double firstNum = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите второе число");
+        double secondNum = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите операцию из списка: '+', '-', '/', '*' ");
+        char oper = Convert.ToChar(Console.ReadLine());
+
+        double res = 0;
+
+        switch (oper)
+        {
+            case '+': 
+                res = firstNum + secondNum; break;
+            case '-':
+                res = firstNum - secondNum; break;
+            case '/':
+                res = firstNum / secondNum; break;
+            case '*':
+                res = firstNum * secondNum; break;
+        }
+
+        Console.WriteLine("Результат операции: " +  res);
+    }
+}
